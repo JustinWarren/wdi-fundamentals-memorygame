0f0ed37f19bc9b725acaf1de605ea4593cc8fd64
Justin <justin.nathaniel.warren@gmail.com>
2019-09-04 15:25:43 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.")
+		console.log("Sorry, try again.");
 	}
 };
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
 
+//Function that stores steps when user flips card
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	
+	if (cardsInPlay.length === 2) {
+
+	checkForMatch();
+	};
+	console.log("User flipped " + cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+
+/*  Note:  I haven't been able to get this..need help!
+
+	
+	My console display:
+	User flipped queen
+	Sorry, try again
+	User flipped king
+
+	It is supposed to display:
+	User flipped queen
+	User flipped king
+	Sorry, try again.
+
+
+*/
